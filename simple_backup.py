df5fef72d0f079ceaf100c5452d890cb0ec5d913
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-11 00:58:10 -0400

    Initialized todo list prior to coding

---
+++ simple_backup.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+# TODO: Import the required modules.
+    # - [ ] Define the source directory where the files to be backed up are located.
+    # - [ ] Define the destination directory where the backup files will be stored.
+    # - [ ] Traverse the source directory and identify all files to be backed up.
+    # - [ ] Implement the file backup mechanism to copy files from the source directory to the destination directory.
+    # - [ ] Handle cases where a file with the same name already exists in the destination directory.
+    # - [ ] Display the progress of the backup process, showing the number of files backed up and the total size.
+    # - [ ] Implement error handling to deal with file access errors or insufficient permissions.
+    # - [ ] Log the backup operations and any encountered errors for future reference or troubleshooting.
+    # - [ ] Provide a backup summary upon completion, including the number of files backed up and the total backup size.
+    # - [ ] Validate user input to ensure the provided directories exist and are accessible.
+    # - [ ] Design a user-friendly interface to interact with the program, whether through a command-line or graphical interface.
